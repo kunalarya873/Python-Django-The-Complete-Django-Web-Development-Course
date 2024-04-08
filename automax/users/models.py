c731c5e63bbc5dcbc1688cebd7e5af8db7624a18
kunalarya873 <kunalarya873@gmail.com>
2024-04-08 22:08:05 +0530

    there are some errors

--- automax/users/models.py
+++ automax/users/models.py
@@ -17,5 +17,5 @@ class Profile(models.Model):
     location = models.OneToOneField(Location, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f"{self.user.username}'s Profile"
+        return f"{self.user.username}\'s Profile"
 
